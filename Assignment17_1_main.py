5d3fe94a553d41897ade3949c99f4c56f1fe4047
dalalarya <dalalarya@icloud.com>
2026-01-27 01:24:31 +0530

    Add files via upload

---
+++ Assignment17_1_main.py
@@ -0,0 +1,10 @@
+
+import Assignment17_1_arithematic
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print(Assignment17_1_arithematic.Add(x,y))
+print(Assignment17_1_arithematic.Sub(x,y))
+print(Assignment17_1_arithematic.Mult(x,y))
+print(Assignment17_1_arithematic.Div(x,y))
